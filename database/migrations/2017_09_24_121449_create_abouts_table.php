51123b3679af7115d51720f617cc9874fe699fff
Valentyn Sudeikin <val85@voliacable.com>
2017-09-24 15:47:52 +0300

    added Models, Migrations

---
+++ database/migrations/2017_09_24_121449_create_abouts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->date('date');
+            $table->string('phone', 20);
+            $table->string('img', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
